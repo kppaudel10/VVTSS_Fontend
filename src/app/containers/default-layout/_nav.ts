8045c5e8fff03caf7a112fdd7c79cdd7166e4f9d
ramesh.mandal <mandalramesh816@gmail.com>
2023-05-04 07:10:45 +0545

    feat;

---
+++ src/app/containers/default-layout/_nav.ts
@@ -0,0 +1,41 @@
+import { INavData } from '@coreui/angular';
+
+export const navItems: INavData[] = [
+  {
+    name: 'Dashboard',
+    url: '/dashboard',
+    iconComponent: { name: 'cil-speedometer' },
+    badge: {
+      color: 'info',
+      text: 'NEW'
+    }
+  },
+  
+  {
+    name: 'VVTA Page',
+    title: true
+  },
+  {
+    name: 'Pages',
+    url: '/login',
+    iconComponent: { name: 'cil-star' },
+    children: [
+      {
+        name: 'Login',
+        url: '/login'
+      },
+      {
+        name: 'Register',
+        url: '/register'
+      },
+      {
+        name: 'Error 404',
+        url: '/404'
+      },
+      {
+        name: 'Error 500',
+        url: '/500'
+      }
+    ]
+  },
+];
